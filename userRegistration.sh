43504ebe7b3b3f30d785539991cc5113dd29866e
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-27 16:56:28 +0530

    Added:-User need to enter a valid name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 echo "Welcome To User Registration Programming"
 
+validName="^[A-Z][a-z]{3,}$"
+userName=""
+read -p "Enter The Name" userName
+if [[ $userName =~ $validName ]]
+then
+      echo "User Name Is Valid"
+else
+      echo "User Name Is Invalid"    
+fi

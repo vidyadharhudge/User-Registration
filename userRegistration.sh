b2fdbc3e79539c06d38ef4d7b854eb478f682ed3
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-27 17:21:43 +0530

    Added:-Enter valid Last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -x
 echo "Welcome To User Registration Programming"
 
-validName="^[A-Z][a-z]{3,}$"
+validName=^[A-Z][a-z]{3,}* #for first name
+validName=[A-Z][a-z]{3,}*$ #for last name
+validName="^[A-Z][a-z]{3,}[ ]*[A-Z][a-z]{3,}*$"  #merging first name and last name [ ] for space  
 userName=""
-read -p "Enter The Name" userName
+read -p "Enter The Name:" userName
 if [[ $userName =~ $validName ]]
 then
       echo "User Name Is Valid"

9809fe36b0d5ee4924a9f7965dad278245b74ab9
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-10 16:29:14 +0530

    MODIFIED;-User Registration Program Using Function

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,55 +1,40 @@
-#!/bin/bash -x
+
+#!/bin/bash-x 
 echo "Welcome To User Registration Programming"
-validName="^[A-Z][a-z]{3,}$" 
 
-firstName=""
-read -p "Enter The firstName:" firstName
-if [[ $firstName =~ $validName ]]
-then
-      echo "firstName Is Valid"
-else
-      echo "firstName Is Invalid"    
-fi
-
-lastName=""
-read -p "Enter The lastName:" lastName
-if [[ $lastName =~ $validName ]]
-then
-      echo "lastName Is Valid"
-else
-      echo "lastName Is Invalid"
-fi
-
-email=""
 emailPattern1="^[a-zA-Z0-9]{3,}+[@]{1}$"
 emailPattern2="^[a-zA-Z0-9]{3,}+[@]{1}[a-zA-Z0-9]{3,}$"
 emailPattern3="^[a-zA-Z0-9]{3,}+[@]{1}[a-zA-Z0-9]{3,}+[.][a-zA-Z]{3,}$"
-validEmail="^[a-zA-Z0-9]{3,}+[@][a-zA-Z0-9]{3,}+[.][a-zA-Z]{3,}$"
+
+#CONSTANTS
+VALIDNAMEPATTERN="^[A-Z][a-z]{3,}$"
+EMAILPATTERN="^[a-zA-Z0-9]{3,}+[@][a-zA-Z0-9]{3,}+[.][a-zA-Z]{3,}$"
+MOBILEPATTERN="^[0-9]{2}[ ][0-9]{10}$" 
+PASSWORD="^[A-Z]{1,}*[a-z]{4,}*[0-9]{2,}*[\!\@\#\%\$\&\*\]{1}*$"
+
+function Pattern_Check()
+{
+
+   if [[ $1 =~ $2 ]]
+   then
+        echo "Valid Pattern"
+   else
+        echo "Invalid Pattern"
+   fi
+}
+
+read -p "Enter The firstName:" firstName
+Pattern_Check $firstName $VALIDNAMEPATTERN
+
+read -p "Enter The lastName:" lastName
+Pattern_Check $lastName $VALIDNAMEPATTERN
+
 read -p "Enter The Email:" email
-if [[ $email =~ $validEmail ]]
-then
-      echo "email is valid"
-else
-      echo "email is invalid"
-fi
+Pattern_Check $email $EMAILPATTERN
 
-mobileFormat=""
-validMobileformat="^[0-9]{2,}[ ][0-9]{10,}$"   #[ ] for space 
 read -p "Enter the mobile number format:" mobileFormat
-if [[ $mobileFormat =~ $validMobileformat ]]
-then
-      echo "The mobile format is valid"
-else
-      echo "The mobile format is invalid"
-fi
+Pattern_Check $mobileFormat $MOBILEPATTERN
 
-Password=""
-validPassword="^[A-Z]{1,}*[a-z]{4,}*[0-9]{2,}*[\!\@\#\%\$\&\*\]{1}*$"
 read -p "Enter the valid password:" Password
-if [[ $Password =~ $validPassword ]]
-then
-      echo "The password is valid"
-else
-      echo "The password is not valid"
-fi
+Pattern_Check $Password $PASSWORD
 

f470e73b266a081b6c6480871c8734f9cea39a5c
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-04-10 23:08:53 +0530

    Refactor:-Adding The Commit Message

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,40 +1,44 @@
+#!/bin/bash -x
+echo "Welcome to user registration problem."
 
-#!/bin/bash-x 
-echo "Welcome To User Registration Programming"
-
+#for seprate email validations
 emailPattern1="^[a-zA-Z0-9]{3,}+[@]{1}$"
 emailPattern2="^[a-zA-Z0-9]{3,}+[@]{1}[a-zA-Z0-9]{3,}$"
 emailPattern3="^[a-zA-Z0-9]{3,}+[@]{1}[a-zA-Z0-9]{3,}+[.][a-zA-Z]{3,}$"
 
-#CONSTANTS
-VALIDNAMEPATTERN="^[A-Z][a-z]{3,}$"
-EMAILPATTERN="^[a-zA-Z0-9]{3,}+[@][a-zA-Z0-9]{3,}+[.][a-zA-Z]{3,}$"
-MOBILEPATTERN="^[0-9]{2}[ ][0-9]{10}$" 
-PASSWORD="^[A-Z]{1,}*[a-z]{4,}*[0-9]{2,}*[\!\@\#\%\$\&\*\]{1}*$"
+#constants
+VALIDNAMEPATTERN="^[A-Z][a-z]{2,}$"
+EMAILPATTERN="^[a-zA-Z0-9]{3,}+[@]{1}[a-zA-Z0-9]{3,}+[.][a-zA-Z]{3,}$"
+MOBILEPATTERN="^[0-9]{2}[ ][0-9]{10}$" #[ ]for one space
+PASSWORD="^[A-Z]{1,}*[a-z]{4,}[*&^%$#@!+=]{1}*[0-9]{2,}*$"
 
-function Pattern_Check()
+#function for pattern check
+function patternCheck()
 {
-
    if [[ $1 =~ $2 ]]
    then
-        echo "Valid Pattern"
+      echo "Valid Pattern."
    else
-        echo "Invalid Pattern"
+      echo "Invalid Pattern."
    fi
 }
 
-read -p "Enter The firstName:" firstName
-Pattern_Check $firstName $VALIDNAMEPATTERN
+#for user input and calling the function and passes the parameter
+read -p "Enter first name: " firstName
+patternCheck $firstName $VALIDNAMEPATTERN
 
-read -p "Enter The lastName:" lastName
-Pattern_Check $lastName $VALIDNAMEPATTERN
+read -p "Enter last name: " lastName
+patternCheck $lastName $VALIDNAMEPATTERN
 
-read -p "Enter The Email:" email
-Pattern_Check $email $EMAILPATTERN
+read -p "Enter email address: " emailId
+patternCheck $emailId $EMAILPATTERN
 
-read -p "Enter the mobile number format:" mobileFormat
-Pattern_Check $mobileFormat $MOBILEPATTERN
+read -p "Enter mobile number: " mobileNumber
+patternCheck $mobileNumber $MOBILEPATTERN
 
-read -p "Enter the valid password:" Password
-Pattern_Check $Password $PASSWORD
+read -p "Enter your password: " password
+patternCheck $password $PASSWORD
+
+
+
 
